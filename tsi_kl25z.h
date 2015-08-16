ded2dbd546586520c1d996cf9c7b990427434778
Milton Sierra Solano <milton86@gmail.com>
2015-08-15 21:51:54 -0300

    Update tsi_kl25z.h

--- tsi_kl25z.h
+++ tsi_kl25z.h
@@ -19,11 +19,20 @@
 /*! @brief Device instance number */
 #define TSI_INSTANCE                0
 
-void TSI0_IRQHandler(void);
+void TSI0_IRQHandler(void); //Function for tsi interrupt
 
-
+/** \fn initHwConfigTSI()
+ *  \brief Init the TSI setinghs for FRDM-KL25Z
+ *  \param void
+ *  \return void
+ */ 
 void initHwConfigTSI(void);
 
+/** \fn averageMeasurement(samples)
+ *  \brief Perform n samples and return the avg of ch9 and ch10
+ *  \param uint8_t
+ *  \return uint32_t
+ */
 uint32_t averageMeasurement(uint8_t samples);
 
 #endif /* SOURCES_TSI_KL25Z_H_ */
